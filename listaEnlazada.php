2b3984ebf70351985e616b4892bfcf9ba9e90c96
Johann-Cristancho <johancri2010@gmail.com>
2025-05-05 15:47:46 -0500

    Add files via upload

---
+++ listaEnlazada.php
@@ -0,0 +1,47 @@
+<?php 
+
+class Nodo {
+    public $dato;
+    public $siguiente;
+
+    public function __construct($dato) {
+        $this->dato = $dato;
+        $this->siguiente = null;
+    }
+}
+
+class listaenlazada {
+    public $inicio;
+
+    public function __construct() {
+        $this->inicio = null;
+    }
+
+    public function insertar($dato) {
+        $nuevoNodo = new Nodo($dato);
+        $nuevoNodo->siguiente = $this->inicio;
+        $this->inicio = $nuevoNodo;
+    }
+
+    public function imprimirHTML() {
+        $actual = $this->inicio;
+        echo "<ul>";
+
+        while ($actual != null) {
+            echo "<li>" . $actual->dato . "</li>"; // también corregido: <li> sin cierre adecuado
+            $actual = $actual->siguiente;
+        }
+
+        echo "</ul>";
+    }
+}
+
+$lista = new listaenlazada();
+
+$lista->insertar("johan");
+$lista->insertar("Felipe");
+$lista->insertar("Karol");
+
+$lista->imprimirHTML();
+
+?>
